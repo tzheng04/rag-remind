0607164c3f251d8aafea691bf91eb17f01d8173c
Tony Zheng <tzheng12@umd.edu>
2026-09-25 16:14:44 -0400

    Added recurring_reminders.sql to support user reminders on intervals

---
+++ src/sql/recurring_reminders.sql
@@ -0,0 +1,24 @@
+CREATE TABLE recurring_reminders (
+    id SERIAL PRIMARY KEY,
+    message_id INTEGER REFERENCES messages(id),
+    author_id BIGINT NOT NULL,
+
+    title TEXT NOT NULL,
+    reminder_type TEXT NOT NULL,
+
+    frequency TEXT NOT NULL,
+    interval_value INTEGER NOT NULL DEFAULT 1,
+
+    weekday INTEGER,
+    day_of_month INTEGER,
+    month INTEGER,
+
+    hour INTEGER,
+    minute INTEGER,
+
+    start_date DATE,
+    end_date DATE,
+    
+    next_reminder TIMESTAMPTZ,
+    active BOOLEAN NOT NULL DEFAULT TRUE
+);
